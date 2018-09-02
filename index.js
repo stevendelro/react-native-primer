aa1187b42bf25815bf0289210b3d94423b60fd8c
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-02 10:32:57 -0700

    Initialized AlbumList component, added to App

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/Components/Header';
+import AlbumList from './src/Components/AlbumList';
 
 // Create a component
-const App = () => <Header headerText={'Movie Finder'} />;
+const App = () => (
+  <View>
+   <Header headerText={'Albums'} />
+   <AlbumList />
+  </View>
+);
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
