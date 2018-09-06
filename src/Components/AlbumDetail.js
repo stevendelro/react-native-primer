93aea263a53323281795decb86d80b445574eaf2
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-06 12:07:38 -0700

    Added header info, artist image and album image sections to albumList

--- src/Components/AlbumDetail.js
+++ src/Components/AlbumDetail.js
@@ -1,16 +1,68 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, image, thumbnail_image } = album;
+  const {
+    artistImageStyle,
+    albumImageStyle,
+    headerTextStyle,
+    headerContentStyle,
+    thumbnailContainerStyle
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={artistImageStyle}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      
+      <CardSection>
+        <Image source={{ uri: image }} style={albumImageStyle} />
+      </CardSection>
+
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  artistImageStyle: {
+    height: 50,
+    width: 50
+  },
+  albumImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
 };
 
 export default AlbumDetail;
